8eace3a3a4ebc4cedcd637d8bb18646355b91a86
Itswali <luckywali0986@gmail.com>
2026-01-16 16:57:46 +0500

    starting with Personal Task and Focus Hub

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import './App.css'
-import CreateTask from './components/CreateTask'
-// import Dashboard from './components/Dashboard'
-// import TaskManager from './components/TaskManager'
 
 function App() {
 
   return (
     <>
-    {/* <TaskManager /> */}
-    {/* <Dashboard /> */}
-    <CreateTask />
 
     </>
   )
